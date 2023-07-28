50a9545ee550763818f4d870421425d53065a8b4
abhay <kadamabhay40@gmail.com>
2023-07-29 01:30:06 +0530

    Added:mutual fund links

--- main.py
+++ main.py
@@ -28,63 +28,67 @@ app.add_middleware(
 )
 
 
-
 @app.get("/")
 def read_welcome():
-    return{"status":200,"message":"Welcome to Stock api"}
+    return {"status": 200, "message": "Welcome to Stock api"}
 
 
-#Stock Apis
+# Stock Apis
+
 
 @app.get("/allindices")
 def read_root():
     list = handl_all_indices()
     return list
 
+
 @app.get("/topstocks/{cat}")
-def read_top_stocks(cat,skip:int=0,limit:int=10):
-    list= handl_top_stocks(cat)
-    return list[skip:skip+limit]
+def read_top_stocks(cat, skip: int = 0, limit: int = 10):
+    list = handl_top_stocks(cat)
+    return list[skip : skip + limit]
+
 
 @app.get("/sector-wise-data/{sector}")
 def read_sector_wise_data(sector):
     list = handl_sector(sector)
     return list
 
+
 @app.get("/fifty-two-week-data/{status}")
 def read_fifty_two_week_data(status):
-    list=handl_year_high(status)
+    list = handl_year_high(status)
     return list
+
 
 @app.get("/stock-by-rsi-dma/{cat}")
 def read_stocks_rsi_dma(cat):
-    list=handl_dma_rsi(cat)
+    list = handl_dma_rsi(cat)
     return list
 
 
-#get all it,pharma etc stock
+# get all it,pharma etc stock
 @app.get("/theme-based-stocks/{cat}")
 def read_theme_based_stocks(cat):
-    list=handl_themebase_stocks(cat)
+    list = handl_themebase_stocks(cat)
     return list
+
 
 @app.get("/stockcollection/{cat}")
 def read_stock_collection(cat):
-    list=handl_stock_collection(cat)
+    list = handl_stock_collection(cat)
     return list
 
 
 @app.get("/stock-details/{stock}/{stock_code}")
-def read_stock_details(stock,stock_code):
-    list=handl_stock_details(stock,stock_code)
+def read_stock_details(stock, stock_code):
+    list = handl_stock_details(stock, stock_code)
     return list
 
 
-
-#get all gainers, losers , 52 week high low
+# get all gainers, losers , 52 week high low
 @app.get("/all-top-stocks/{cat}")
 def read_all_top_stocks(cat):
-    list=hadle_top_rated(cat)
+    list = hadle_top_rated(cat)
     return list
 
 
@@ -93,30 +97,36 @@ def read_one_day_hist(stock_symbol):
     list = hadle_one_day_history(stock_symbol)
     return list
 
+
 @app.get("/one-week-hist/{stock_symbol}")
 def read_one_week_hist(stock_symbol):
     list = hanle_one_week(stock_symbol)
     return list
+
 
 @app.get("/one-month-hist/{stock_symbol}")
 def read_one_month_hist(stock_symbol):
     list = hanle_one_month(stock_symbol)
     return list
 
+
 @app.get("/one-year-hist/{stock_symbol}")
 def read_one_year_hist(stock_symbol):
     list = handl_one_year(stock_symbol)
     return list
+
 
 @app.get("/three-year-hist/{stock_symbol}")
 def read_three_year_hist(stock_symbol):
     list = handl_three_year(stock_symbol)
     return list
 
+
 @app.get("/five-year-hist/{stock_symbol}")
 def read_five_year_hist(stock_symbol):
     list = handl_five_years(stock_symbol)
     return list
+
 
 @app.get("/stock-details-all/{name}")
 def read_stock_all_details(name):
@@ -124,14 +134,16 @@ def read_stock_all_details(name):
     return list
 
 
-#Mutual fund apis
+# Mutual fund apis
 
-@app.get("/get-mutual-fund/{mutualfund}")
-def read_all_mutual_fund_data(mutualfund):
-    list= handl_all_MutualFund_data(mutualfund)
+
+@app.get("/get-mutual-fund/{mutualfund}/{code}")
+def read_all_mutual_fund_data(mutualfund, code):
+    list = handl_all_MutualFund_data(mutualfund, code)
     return list
+
 
 @app.get("/get-mutual-fund-history/{code}")
 def read_all_mutual_fund_history(code):
-    list= getMutualfund_history_chart(code)
+    list = getMutualfund_history_chart(code)
     return list
